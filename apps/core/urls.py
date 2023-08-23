51baa25ea6a0ffe77e15afac06263117cf2c7276
Gabriel Penaranda <gabrielpg77@gmail.com>
2023-08-23 17:36:38 -0400

    Ajustes en layouts

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import index, sistema
+from .views import index
 
 app_name = 'core'
 
 urlpatterns = [
     path('', index, name='core-index'),
-    path('admin/', sistema, name='core-sistema'),
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
+
+@login_required
 def index(request):
     return render(request, 'core/index.html')
 
 
-@login_required
+"""@login_required
 def sistema(request):
-    return render(request, 'core/sistema.html')
+    return render(request, 'core/sistema.html')"""
 
